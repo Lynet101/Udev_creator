b8b8a96f89a400d4b3e4efa467e3c135354d23ca
Sebastian Lindau-Skands <47390147+Lynet101@users.noreply.github.com>
2023-09-05 08:06:07 +0200

    Does not support NixOS yet

--- udev_rule_maker.py
+++ udev_rule_maker.py
@@ -1,72 +1,67 @@
 import os
 from subprocess import check_output as cop
+from sys import argv as user_arguments
 
 #Vendor ID: i[23:27]
 #Name: i[33:]
 
-class Udev(object):
-    def __init__(self, perm_lvl, distro):
-        self.perm_lvl = perm_lvl
-        self.usb_devices = []
-        self.idVendor = "idVendor" #will make sense later
-        self.search()
+#perm_lvl 1 = 0664
+#perm_lvl 2 = 0666
+
+class Program():
+    def __init__(self):
+        self.main()
+        self.scan()
         self.select()
-        if distro == 1:
-            self.save_nix()
-        elif distro == 2:
-            self.save_classic()
+        self.save_classic()
+    
+    def main(self):
+        try:
+            self.manufactor = user_arguments[1]
+        except:
+            self.manufactor = "" 
+        try:
+            self.permission_lvl = str(user_arguments[2])
+        except:
+            self.permission_lvl = "0664"
 
-    def search(self):
-        query = input("\nEnter manufactor name (type CC for CharaChorder)\nLeave blank to display all devices: ")
-        if query == 'CC' or query == 'cc':
-            query = "CharaChorder"
-        output = str(cop(f"lsusb | grep '{query}'", shell=True)).replace("b'", "").replace("\\n", "\n")
+    def scan(self):
+        try:
+            output = str(cop(f"lsusb | grep '{self.manufactor}'", shell=True)).replace("b'", "").replace("\\n", "\n")
+        except:
+            print(f"no devices found with manufactor {self.manufactor}")
+            exit()
         self.usb_devices = output.split("\n")
-
+    
     def select(self):
-        print("\nPlease select the correct device, from the list below:")
-        for ind, i in enumerate(self.usb_devices):
-            print(f'{ind}: {i[33:]}')
-        self.device = self.usb_devices[int(input("\nSelect device: "))]
+        for ind, device in enumerate(self.usb_devices):
+            print(f'{ind}: {device[33:]}')
+        
+        user_select = int(input(f"Select device: "))
+        selected_device = self.usb_devices[user_select]
+        self.device_id = selected_device[23:27]
 
     def save_classic(self):
-        save_path = f'/etc/udev/rules.d/60-{self.device[33:].replace(" ", "_")}.rule'
-        udev_rule = f'SUBSYSTEM=="usb", ATTR{self.idVendor}=="[{self.device[23:27]}]", MODE="{self.perm_lvl}", GROUP="plugdev"'
+        self.idVendor = "idVendor"
+        os.system(f'echo SUBSYSTEM=="usb", ATTR{self.idVendor}=="[{self.device_id}]", MODE="{self.permission_lvl}", GROUP="plugdev" >> /etc/udev/rules.d/60-usb_rules-lynet101.rules')
+        print(f"Device {self.device_id} with permission {self.permission_lvl} saved")
 
-        print(f'\nshould the following be written to {save_path} (y/n)? \n "{udev_rule}"')
-        temp = input(": ")
-        if temp == 'y' or temp == 'Y':
-            try:
-                os.system(f'echo {udev_rule} > {save_path}')
-            except:
-                print("an error occured whilst saving (are you sudo?)")
-        elif temp == 'n' or temp == 'N':
-            print("Okay, no changes made")
-
-    def save_nix(self):
-        os.system(f"echo ")
-
-
-def main():
-    os.system("clear")
+def help():
     print("////////////////////////////////////////////////////////////")
-    print("///////////////Udev Rule maker, by Lynet_101////////////////")
+    print("///////////////Udev Creator, by Lynet_101////////////////")
     print("////////////////////////////////////////////////////////////")
-    
-    print("\nAre you using NixOS, or another gnu/linux distro?")
-    distro = int(input("1) I'm using NixOS | 2) I'm using a different Linux distro | 3) I'm not using Linux\n: "))
 
-    if distro == 3:
-        print("Only linux users!")
-        return
-
-    print("\nWhat permission level would you like to use?")
-    perm_lvl = int(input("1) Read & Write permision for members of udev group (recommended)\n2) Read & Write permission for All (Not recommended)\n: "))
-    if perm_lvl == 1:
-        perm_lvl = "0664"
-    elif perm_lvl == 2:
-        perm_lvl = "0666"
-    Udev(perm_lvl, distro)
+    print("Usage: udev_creator [manufactor] [permission_lvl]")
+    print("Example: udev_creator CharaChorder 0666")
+    print("If manufactor is not given, all devices will be scanned")
+    print("If permission_lvl is not given, 0664 will be used")
 
 if __name__ == "__main__":
-    main()
+    try:
+        if user_arguments[1] == "help":
+            help()
+        else:
+            Program()
+    except:
+        Program()
+        
